8bc883f9292fa1e9b760e1cbd43461bd7fd4096c
Kev45 <50998270+Kev45@users.noreply.github.com>
2019-05-24 19:40:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const cfg require('./index.json'); // a garder en version desktop
+const cfg require('./index.json');
+const token = process.env.token;
 const prefix = ("?");
 
 bot.on('ready', function () {
@@ -39,5 +40,4 @@ bot.on('message', msg => {
 
 });
 
-bot.login(token);
 bot.login(token);
